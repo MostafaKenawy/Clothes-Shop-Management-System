ef7933ac2f810c83e8a3a70c6798bbfc4284d3d9
Mostafa Kenawy <58022390+MostafaKenawy@users.noreply.github.com>
2022-01-04 07:14:39 +0200

    Update clothing_db.sql

--- database/clothing_db.sql
+++ database/clothing_db.sql
@@ -1,281 +1,253 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 05, 2020 at 01:37 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
+-- drop table if exists `clothing_db`;
+-- create database `clothing_db`;
+-- use `clothing_db`;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `clothing_db`
---
+-- we created the database in php files
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `items`
 --
-
-CREATE TABLE `items` (
-  `id` int(30) NOT NULL,
-  `item_code` varchar(100) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `description` text NOT NULL,
-  `size` varchar(10) NOT NULL,
-  `price` float NOT NULL,
-  `date_created` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+drop table if exists `items`;
+create table `items` (
+  `id` int(30) not null,
+  `item_code` varchar(100) not null,
+  `name` varchar(200) not null,
+  `description` text not null,
+  `size` varchar(10) not null,
+  `price` float not null,
+  `date_created` datetime not null default current_timestamp()
+);
 --
 -- Dumping data for table `items`
 --
-
-INSERT INTO `items` (`id`, `item_code`, `name`, `description`, `size`, `price`, `date_created`) VALUES
+insert into `items` (`id`, `item_code`, `name`, `description`, `size`, `price`, `date_created`) values
 (1, '309874256222', 'Sample Shirt', 'Sample only', 'M', 500, '2020-11-04 09:51:01'),
 (2, '774420957142', 'sample 1', 'sample 1', 'XS', 250, '2020-11-04 10:58:59');
+
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `receiving`
 --
-
-CREATE TABLE `receiving` (
-  `id` int(30) NOT NULL,
-  `supplier_id` int(30) NOT NULL,
-  `description` text NOT NULL,
-  `total_cost` float NOT NULL,
-  `inventory_ids` text NOT NULL,
-  `date_created` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+drop table if exists `receiving`;
+create table `receiving` (
+  `id` int(30) not null,
+  `supplier_id` int(30) not null,
+  `description` text not null,
+  `total_cost` float not null,
+  `inventory_ids` text not null,
+  `date_created` datetime not null default current_timestamp()
+);
 --
 -- Dumping data for table `receiving`
 --
-
-INSERT INTO `receiving` (`id`, `supplier_id`, `description`, `total_cost`, `inventory_ids`, `date_created`) VALUES
+insert into `receiving` (`id`, `supplier_id`, `description`, `total_cost`, `inventory_ids`, `date_created`) values
 (1, 1, '', 215000, '7,9', '2020-11-04 14:56:18');
+
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `sales`
 --
-
-CREATE TABLE `sales` (
-  `id` int(30) NOT NULL,
-  `user_id` int(30) NOT NULL,
-  `total_amount` float NOT NULL,
-  `amount_tendered` int(30) NOT NULL,
-  `inventory_ids` text NOT NULL,
-  `date_created` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+drop table if exists `sales`;
+create table `sales` (
+  `id` int(30) not null,
+  `user_id` int(30) not null,
+  `total_amount` float not null,
+  `amount_tendered` int(30) not null,
+  `inventory_ids` text not null,
+  `date_created` datetime not null default current_timestamp()
+);
 --
 -- Dumping data for table `sales`
 --
-
-INSERT INTO `sales` (`id`, `user_id`, `total_amount`, `amount_tendered`, `inventory_ids`, `date_created`) VALUES
+insert into `sales` (`id`, `user_id`, `total_amount`, `amount_tendered`, `inventory_ids`, `date_created`) values
 (1, 1, 1500, 1500, '10,11', '2020-11-05 08:36:52'),
 (2, 1, 750, 1000, '12', '2020-11-05 08:37:13');
+
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `stocks`
 --
-
-CREATE TABLE `stocks` (
-  `id` int(30) NOT NULL,
-  `item_id` int(30) NOT NULL,
-  `type` tinyint(1) NOT NULL COMMENT '1= in,2=out',
-  `qty` int(30) NOT NULL,
-  `price` float NOT NULL,
-  `date_created` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+drop table if exists `stocks`;
+create table `stocks` (
+  `id` int(30) not null,
+  `item_id` int(30) not null,
+  `type` tinyint(1) not null comment '1= in,2=out',
+  `qty` int(30) not null,
+  `price` float not null,
+  `date_created` datetime not null defualt current_timestamp()
+);
 --
 -- Dumping data for table `stocks`
 --
-
-INSERT INTO `stocks` (`id`, `item_id`, `type`, `qty`, `price`, `date_created`) VALUES
+insert into `stocks` (`id`, `item_id`, `type`, `qty`, `price`, `date_created`) values
 (7, 2, 1, 500, 250, '2020-11-04 15:55:03'),
 (9, 1, 1, 500, 180, '2020-11-04 15:55:29'),
 (10, 2, 2, 2, 250, '2020-11-05 08:36:52'),
 (11, 1, 2, 2, 500, '2020-11-05 08:36:52'),
 (12, 2, 2, 3, 250, '2020-11-05 08:37:13');
 
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `suppliers`
 --
-
-CREATE TABLE `suppliers` (
-  `id` int(30) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `address` text NOT NULL,
-  `contact` varchar(50) NOT NULL,
-  `date_created` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+drop table if exists `suppliers`;
+create table `suppliers` (
+  `id` int(30) not null,
+  `name` varchar(200) not null,
+  `address` text not null,
+  `contact` varchar(50) not null,
+  `date_created` datetime not null default current_timestamp()
+);
 --
 -- Dumping data for table `suppliers`
 --
-
-INSERT INTO `suppliers` (`id`, `name`, `address`, `contact`, `date_created`) VALUES
+insert into `suppliers` (`id`, `name`, `address`, `contact`, `date_created`) values
 (1, 'ABC Apparel', 'CBD St., EFG City', '+6948 8542 623', '2020-11-04 09:33:26'),
 (2, 'Men Apparel', 'Sample Address', '65524556', '2020-11-04 09:33:48'),
 (3, 'Ladies Apparel', 'Company address', '65524556', '2020-11-04 09:34:15'),
 (4, 'Trends Apparel', 'Sample Address', '8747808787', '2020-11-04 09:34:37');
+
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `system_settings`
 --
-
-CREATE TABLE `system_settings` (
-  `id` int(30) NOT NULL,
-  `name` text NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `contact` varchar(20) NOT NULL,
-  `cover_img` text NOT NULL,
-  `about_content` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+drop table if exists `system_settings`; 
+create table `system_settings` (
+  `id` int(30) not null,
+  `name` text not null,
+  `email` varchar(200) not null,
+  `contact` varchar(20) not null,
+  `cover_img` text not null,
+  `about_content` text not null
+);
 
 --
 -- Dumping data for table `system_settings`
 --
 
-INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `cover_img`, `about_content`) VALUES
+insert into `system_settings` (`id`, `name`, `email`, `contact`, `cover_img`, `about_content`) value
 (1, 'Clothing Store Management System', '', '', '', '');
+
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
-
-CREATE TABLE `users` (
-  `id` int(30) NOT NULL,
-  `name` text NOT NULL,
-  `username` varchar(200) NOT NULL,
-  `password` text NOT NULL,
-  `type` tinyint(1) NOT NULL COMMENT '1=Admin,2=Staff'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+drop table if exists `users` ;
+create table `users` (
+  `id` int(30) not null,
+  `name` text not null,
+  `username` varchar(200) not null,
+  `password` text not null,
+  `type` tinyint(1) not null comment '1=Admin,2=Staff'
+);
 --
 -- Dumping data for table `users`
 --
-
-INSERT INTO `users` (`id`, `name`, `username`, `password`, `type`) VALUES
+insert into `users` (`id`, `name`, `username`, `password`, `type`) values
 (1, 'admin', 'admin', '0192023a7bbd73250516f069df18b500', 1);
 
 --
--- Indexes for dumped tables
 --
-
+-- -----------------------------------------Indexes for dumped tables
+--
 --
 -- Indexes for table `items`
 --
-ALTER TABLE `items`
-  ADD PRIMARY KEY (`id`);
-
+alter table `items`
+  add primary key (`id`);
+  
 --
 -- Indexes for table `receiving`
 --
-ALTER TABLE `receiving`
-  ADD PRIMARY KEY (`id`);
-
+alter table `receiving`
+  add primary key (`id`);
+  
 --
 -- Indexes for table `sales`
 --
-ALTER TABLE `sales`
-  ADD PRIMARY KEY (`id`);
-
+alter table `sales`
+  add primary key (`id`);
+  
 --
 -- Indexes for table `stocks`
 --
-ALTER TABLE `stocks`
-  ADD PRIMARY KEY (`id`);
-
+alter table `stocks`
+  add primary key (`id`);
+  
 --
 -- Indexes for table `suppliers`
 --
-ALTER TABLE `suppliers`
-  ADD PRIMARY KEY (`id`);
-
+alter table `suppliers`
+  add primary key (`id`);
+  
 --
 -- Indexes for table `system_settings`
 --
-ALTER TABLE `system_settings`
-  ADD PRIMARY KEY (`id`);
-
+alter table `system_settings`
+  add primary key (`id`);
+  
 --
 -- Indexes for table `users`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+alter table `users`
+  add primary key (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- -----------------------------------------AUTO_INCREMENT for dumped tables
 --
-
 --
 -- AUTO_INCREMENT for table `items`
 --
-ALTER TABLE `items`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
+alter table `items`
+  modify `id` int(30) not null auto_increment, auto_increment=3;
+  
 --
 -- AUTO_INCREMENT for table `receiving`
 --
-ALTER TABLE `receiving`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
+alter table `receiving`
+  modify `id` int(30) not null auto_increment, auto_increment=2;
+  
 --
 -- AUTO_INCREMENT for table `sales`
 --
-ALTER TABLE `sales`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
+alter table `sales`
+ modify `id` int(30) not null auto_increment, auto_increment=3;
+ 
 --
 -- AUTO_INCREMENT for table `stocks`
 --
-ALTER TABLE `stocks`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
+alter table `stocks`
+  modify `id` int(30) not null auto_increment, auto_increment=13;
+  
 --
 -- AUTO_INCREMENT for table `suppliers`
 --
-ALTER TABLE `suppliers`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
+alter table `suppliers`
+  modify `id` int(30) not null auto_increment, auto_increment=5;
+  
 --
 -- AUTO_INCREMENT for table `system_settings`
 --
-ALTER TABLE `system_settings`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
+alter table `system_settings`
+  modify `id` int(30) not null auto_increment, auto_increment=2;
+  
 --
 -- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+alter table `users`
+  modify `id` int(30) not null auto_increment, auto_increment=2;
+commit;
